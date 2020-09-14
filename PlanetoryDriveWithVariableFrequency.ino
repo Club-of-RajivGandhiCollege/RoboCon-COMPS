45fcde45bf54c4c54786e1b0552077cf2736c0d9
Club-of-RajivGandhiCollege <71271397+Club-of-RajivGandhiCollege@users.noreply.github.com>
2020-09-14 20:19:04 +0530

    Add files via upload
    Planetory motor movement with variable arduino frequency to get better result. (Reduces motor noise)

---
+++ PlanetoryDriveWithVariableFrequency.ino
@@ -0,0 +1,53 @@
+#define pwm      3
+#define dir      2
+
+int pwm_value = 0; 
+
+    
+
+void setup(){
+  setPwmFrequency(pwm, 8);
+  pinMode(pwm,OUTPUT);
+  pinMode(dir,OUTPUT);
+
+}
+
+void loop()
+{
+     analogWrite(pwm,pwm_value);
+     digitalWrite(dir,HIGH);   //Forward
+     delay(200);
+     digitalWrite(dir,LOW);   //Forward
+   
+}
+
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
